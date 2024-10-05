d71cb47203579fbdf341083501092a426ad7ddc7
Lucivando Sousa <lucivandosousa@gmail.com>
2024-10-05 14:50:15 -0300

    feat: create navbar component

---
+++ components/NavBar/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="flex flex-col justify-between items-center p-4 bg-zinc-900 text-white gap-4 shadow-neutral-950 shadow-md md:flex-row">
+    <div class="flex items-center gap-4">
+      <img src="http://via.placeholder.com/50" alt="logo" />
+      <h1 class="text-xl font-bold">LojaDeJogos</h1>
+    </div>
+    <Input
+      type="text"
+      placeholder="Buscar jogos..."
+    />
+  </header>
+</template>
+
+<script setup lang="ts">
+import Input from '@/components/Input/Input.vue'
+</script>
