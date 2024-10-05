66476b9aae55d1bddf68d0860e020132ea3ab921
Lucivando Sousa <lucivandosousa@gmail.com>
2024-10-05 14:50:05 -0300

    feat: create input component

---
+++ components/Input/Input.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    :type="props.type"
+    :placeholder="props.placeholder"
+    class='w-full p-2 rounded bg-zinc-800 text-white outline-none transition-colors duration-300 ease-in-out focus:bg-gray-600 md:w-80'
+  />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'text',
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  }
+})
+</script>
