00177834689d7b3f056ffd082c06897fda6bb686
Lucivando Sousa <lucivandosousa@gmail.com>
2024-10-05 14:56:57 -0300

    feat: create footer component

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="bg-zinc-900 text-white py-8 px-4 text-center text-md">
+    <p>Todos os direitos reservados &copy; 2024</p>
+    <p>
+      <a href="https://github.com/lucivandosousa" target="_blank" class="text-amber-500">Visite meu portfólio</a> | <a href="https://www.linkedin.com/in/lucivandosousa" target="_blank" class="text-amber-500">Conecte-se comigo no LinkedIn</a>
+    </p>
+  </footer>
+</template>
+
